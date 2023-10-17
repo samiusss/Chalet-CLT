af691acf3de3142dac17cd6829bb09ae7ff511af
kingmufasa99 <moulay-mostafa.filali.1@ulaval.ca>
2023-10-17 18:38:46 +0000

    Update 29 files
    - /chalCLT/src/test/java/ca/ulaval/glo2004/AppTest.java
    - /chalCLT/src/main/java/ca/ulaval/glo2004/gui/MainWindow.form
    - /chalCLT/src/main/java/ca/ulaval/glo2004/gui/MainWindow.java
    - /chalCLT/src/main/java/ca/ulaval/glo2004/App.java
    - /chalCLT/.gitignore
    - /chalCLT/pom.xml
    - /InterfaceUtilisateur/.idea/workspace.xml
    - /InterfaceUtilisateur/.idea/artifacts/interface_beta.xml
    - /InterfaceUtilisateur/InterfaceUtilisateur/nbactions.xml
    - /InterfaceUtilisateur/InterfaceUtilisateur/pom.xml
    - /InterfaceUtilisateur/InterfaceUtilisateur/src/main/java/Interface/Main.java
    - /InterfaceUtilisateur/InterfaceUtilisateur/src/main/java/Interface/gui/aPLIUS.png
    - /InterfaceUtilisateur/InterfaceUtilisateur/src/main/java/Interface/gui/MainWindow.form
    - /InterfaceUtilisateur/InterfaceUtilisateur/src/main/java/Interface/gui/MainWindow.java
    - /InterfaceUtilisateur/InterfaceUtilisateur/target/InterfaceUtilisateur-1.0-SNAPSHOT.jar
    - /InterfaceUtilisateur/InterfaceUtilisateur/target/classes/Interface/Main.class
    - /InterfaceUtilisateur/InterfaceUtilisateur/target/classes/Interface/gui/MainWindow$1.class
    - /InterfaceUtilisateur/InterfaceUtilisateur/target/classes/Interface/gui/MainWindow$2.class
    - /InterfaceUtilisateur/InterfaceUtilisateur/target/classes/Interface/gui/MainWindow$3.class
    - /InterfaceUtilisateur/InterfaceUtilisateur/target/classes/Interface/gui/MainWindow$4.class
    - /InterfaceUtilisateur/InterfaceUtilisateur/target/classes/Interface/gui/MainWindow$5.class
    - /InterfaceUtilisateur/InterfaceUtilisateur/target/classes/Interface/gui/MainWindow$6.class
    - /InterfaceUtilisateur/InterfaceUtilisateur/target/classes/Interface/gui/MainWindow$7.class
    - /InterfaceUtilisateur/InterfaceUtilisateur/target/classes/Interface/gui/MainWindow.class
    - /InterfaceUtilisateur/InterfaceUtilisateur/target/maven-archiver/pom.properties
    - /InterfaceUtilisateur/InterfaceUtilisateur/target/maven-status/maven-compiler-plugin/compile/default-compile/createdFiles.lst
    - /InterfaceUtilisateur/InterfaceUtilisateur/target/maven-status/maven-compiler-plugin/compile/default-compile/inputFiles.lst
    - /InterfaceUtilisateur/InterfaceUtilisateur/target/maven-status/maven-compiler-plugin/testCompile/default-testCompile/createdFiles.lst
    - /InterfaceUtilisateur/InterfaceUtilisateur/target/maven-status/maven-compiler-plugin/testCompile/default-testCompile/inputFiles.lst

---
+++ InterfaceUtilisateur/InterfaceUtilisateur/src/main/java/Interface/Main.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package Interface;
+
+/**
+ *
+ * @author ismaelsdiri
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        Interface.gui.MainWindow mainWindow = new Interface.gui.MainWindow();
+        mainWindow.setVisible(true);
+
+    }
+    
+}
