b67e3a6a9f0aee7214457b06f0ccff737c9f43e8
OumarRafiou <orbar@ulaval.ca>
2023-10-30 10:39:32 -0400

    test

---
+++ domaine/controleur.java
@@ -0,0 +1,33 @@
+import java.awt.Point;
+import java.util.List;
+
+public class controleur {
+    private Chalet chalet;
+    private murDTO murSelectionne;
+    private accessoireDTO AccessoireSelectionne;
+    private float zoom;
+    private float offset;
+
+    public Controleur (Chalet chalet, murDTO murSelectionne, accessoireDTO AccessoireSelectionne,float zoom, float offset){
+        this.chalet = chalet;
+        this.murSelectionne = murSelectionne;
+        this.AccessoireSelectionne = AccessoireSelectionne;
+        this.zoom = zoom;
+        this.offset = offset;
+    }
+
+    public Chalet getChalet() {
+        return chalet;
+    }
+
+    public murDTO getMurSelectionne() {
+        return murSelectionne;
+    }
+
+    public accessoireDTO getAccessoireSelectionne() {
+        return AccessoireSelectionne;
+    }
+    public float getZoom () {
+        return zoom;
+    }
+}
