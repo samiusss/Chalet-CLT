71d96127e3ef2a76d209daa2733e2a1329c7c05f
sami sabir <sami.sabir.1@ulaval.ca>
2023-11-01 18:54:15 -0400

    test ui

--- src/main/java/ui/MainWindow.java
+++ src/main/java/ui/MainWindow.java
@@ -1,7 +1,8 @@
 package ui;
 
 import javax.swing.*;
-import java.awt.BorderLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class MainWindow extends javax.swing.JFrame {
 
@@ -9,13 +10,19 @@ public class MainWindow extends javax.swing.JFrame {
     private JPanel PanelAffichage;
     private JPanel PanelDroit;
     private JPanel FenetrePrincipale;
-    private JButton button1;
+    private JButton weshButton;
     private JButton button2;
 
     public MainWindow() {
         setContentPane(FenetrePrincipale);
         setSize(500, 500);
         setLocationRelativeTo(null);
+        weshButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                JOptionPane.showMessageDialog(null, "vive l<algerie, isma t nul");
+            }
+        });
     }
 
 }
