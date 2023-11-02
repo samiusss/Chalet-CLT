d788a400dd0e35a2f576c897684f60486491da2e
Oumar Rafiou Barry <orbar@ulaval.ca>
2023-11-02 00:54:35 -0400

    Creation des sommets de fenetre

--- src/main/java/domain/Porte.java
+++ src/main/java/domain/Porte.java
@@ -6,17 +6,20 @@ import java.awt.*;
 import java.util.*;
 import java.util.List;
 
+/*
 public class Porte extends Accessoires{
     private double largeurporte;
     private double hauteurporte;
 
 
 
-    public Porte(String AID, double largeurporte, double hauteurporte) {
+    public Porte(Point point double largeur, double hauteurporte) {
         super(AID);
         this.largeurporte = largeurporte;
         this.hauteurporte = hauteurporte;
     }
 
 
+
 }
+*/
