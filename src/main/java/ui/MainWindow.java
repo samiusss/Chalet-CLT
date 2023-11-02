e832fef4839ad5836e9e82c789a4de5dbf0f4b89
Ismael Sdiri <ismael.sdiri.1@ulaval.ca>
2023-11-01 20:30:56 -0400

    Ajustemet du MainWindow au goût du jour

--- src/main/java/ui/MainWindow.java
+++ src/main/java/ui/MainWindow.java
@@ -1,19 +1,66 @@
 package ui;
 
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.ItemEvent;
+import java.awt.event.ItemListener;
 
 public class MainWindow extends javax.swing.JFrame {
 
     private JPanel PanelGauche;
-    private JPanel PanelAffichage;
-    private JPanel PanelDroit;
+    private JPanel PanelHaut;
+    private JPanel PannelAffichage;
     private JPanel FenetrePrincipale;
+    private JButton UndoButton;
+    private JButton RedoButton;
+    private JLabel NomProjetLabel;
+    private JLabel VueLabel;
+    private JComboBox VueComboBox;
+    private JLabel ChalCLTPanel;
+    private JLabel MurPanelLabel;
+    private JPanel MurLabel;
+    private JLabel ToitPanelLabel;
+    private JPanel ToitPanel;
+    private JComboBox comboBox1;
+    private JComboBox comboBox2;
+    private JComboBox comboBox3;
+    private JComboBox comboBox4;
 
     public MainWindow() {
         setContentPane(FenetrePrincipale);
         setSize(500, 500);
         setLocationRelativeTo(null);
 
+        UndoButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                //Undo();
+            }
+        });
+        RedoButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                //Redo();
+            }
+        });
+        VueComboBox.addItemListener(new ItemListener() {
+            @Override
+            public void itemStateChanged(ItemEvent e) {
+//                if (e.getStateChange() == ItemEvent.SELECTED) {
+//                    // The user has selected an item from the JComboBox
+//                    String selectedItem = (String) comboBox.getSelectedItem();
+//
+//                    // Check if the selected item is the one you want to trigger the "yo()" method
+//                    if ("OptionToTriggerYo".equals(selectedItem)) {
+//                        yo();
+//                    }
+//                }
+            }
+        });
     }
 
+    private void createUIComponents() {
+        // TODO: place custom component creation code here
+    }
 }
