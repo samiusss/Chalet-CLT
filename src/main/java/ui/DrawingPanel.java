b2cf56f7bf41b74759274da4cb0b91d2ace91295
Moulay-Mostafa Filali <moulay-mostafa.filali.1@ulaval.ca>
2023-11-05 17:59:33 -0500

    simplification drawingPanel

--- src/main/java/ui/DrawingPanel.java
+++ src/main/java/ui/DrawingPanel.java
@@ -9,53 +9,34 @@ import java.io.Serializable;
 
 public class DrawingPanel extends JPanel implements Serializable {
 
-    public Dimension getInitialDimension() {
-        return getPreferredSize();
-    }
     private MainWindow mainWindow;
+    private Controleur controleur;
 
-    public DrawingPanel(){
+    public DrawingPanel() {
+        controleur = new Controleur();
+        setPreferredSize(new Dimension(1000, 1000));
     }
-    Controleur controleur = new Controleur();
+
     public DrawingPanel(MainWindow mainWindow) {
         this.mainWindow = mainWindow;
-        this.controleur = new Controleur();
+        controleur = new Controleur();
         setPreferredSize(new Dimension(1000, 1000));
-
-//        JPanel drawingPanel = mainWindow.getPannelAffichage();
-//        drawingPanel.setBorder(new javax.swing.border.BevelBorder(BevelBorder.LOWERED));
-//
-//        int largeurPanel = (int) (java.awt.Toolkit.getDefaultToolkit().getScreenSize().width * 0.85);
-//        int preferredWidth = largeurPanel;
-//        int preferredHeight = (int) (preferredWidth * 0.5);
-//        drawingPanel.setPreferredSize(new Dimension(preferredWidth, preferredHeight));
-//
-//        // drawingPanel.setPreferredSize(new Dimension(largeurPanel, 1));
-//
-//        //int hauteurPanel = (int) (largeurPanel * 0.5);
-//        //initialDimension = new Dimension(largeurPanel, hauteurPanel);
-//
-//        drawingPanel.setVisible(true);
-//        setBorder(new javax.swing.border.BevelBorder(BevelBorder.LOWERED));
-//        int largeurPanel = (int) (java.awt.Toolkit.getDefaultToolkit().getScreenSize().width*0.85);
-//        setPreferredSize(new Dimension(largeurPanel,1));
-//        setVisible(true);
-//        int hauteurPanel = (int)(largeurPanel*0.5);
-//        initialDimension = new Dimension(largeurPanel,hauteurPanel);
     }
+
     @Override
-    protected void paintComponent(Graphics g)
-    {
+    protected void paintComponent(Graphics g) {
         super.paintComponent(g);
-        if (mainWindow != null){
-            Chaletdrawer mainDrawer = new Chaletdrawer(controleur,getInitialDimension());
+        if (mainWindow != null) {
+            Chaletdrawer mainDrawer = new Chaletdrawer(controleur, getPreferredSize());
             mainDrawer.draw(g);
         }
     }
-        public MainWindow getMainWindow() {
+
+    public MainWindow getMainWindow() {
         return mainWindow;
     }
-        public void setMainWindow(MainWindow mainWindow) {
+
+    public void setMainWindow(MainWindow mainWindow) {
         this.mainWindow = mainWindow;
     }
-};
+}
