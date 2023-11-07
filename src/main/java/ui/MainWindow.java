db889c32375a5ad3a8494e661b4981c9c53f24e8
Ismael Sdiri <ismael.sdiri.1@ulaval.ca>
2023-11-06 22:04:37 -0500

    Ajout du mouselister à PannelAffichage

--- src/main/java/ui/MainWindow.java
+++ src/main/java/ui/MainWindow.java
@@ -5,10 +5,7 @@ import domain.Mur;
 
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.awt.event.ItemEvent;
-import java.awt.event.ItemListener;
+import java.awt.event.*;
 
 import static ui.Chaletdrawer.chalet;
 
@@ -263,6 +260,20 @@ public class MainWindow extends javax.swing.JFrame {
             @Override
             public void actionPerformed(ActionEvent e) {
                 String inputText = ToitPaneltabbedPaneDerrierePanelHauteurTextField.getText();
+            }
+        });
+
+        PannelAffichage.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                if (isAddingPorte)
+                {
+                    //addPorte();
+                }
+                if (isAddingFenetre)
+                {
+                    //addFenetre();
+                }
             }
         });
 
